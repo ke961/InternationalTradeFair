ac6893f87f879fc1d2ce30cc26a6536dd35a46ea
Abila Khan <khankeya961@gmail.com>
2026-08-31 21:59:57 +0600

    Update CustomerCareAgent.java
    feat: implement core system architecture and data management for International Trade Fair application

--- src/main/java/keya/internationaltradefairltd/User/CustomerCareAgent.java
+++ src/main/java/keya/internationaltradefairltd/User/CustomerCareAgent.java
@@ -1,12 +1,14 @@
 package keya.internationaltradefairltd.User;
 
-public class CustomerCareAgent extends User{
+public class CustomerCareAgent extends User {
+    private static final long serialVersionUID = 1L;
 
     public CustomerCareAgent() {
+        super();
+        setUserType("Customer Support Agent");
     }
 
-    public CustomerCareAgent(String firstName, String lastName, String password, String confirmPassword, String phoneNumber, String userType) {
-        super(firstName, lastName, password, confirmPassword, phoneNumber, userType);
+    public CustomerCareAgent(String firstName, String lastName, String userName, String password, String confirmPassword, String phoneNumber) {
+        super(firstName, lastName, userName, password, confirmPassword, phoneNumber, "Customer Support Agent");
     }
-
 }
