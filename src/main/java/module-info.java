89cdbdc9d52214d4235238927bfe4074dfb494e0
Abila Khan <khankeya961@gmail.com>
2026-08-31 22:00:08 +0600

    Update module-info.java
    feat: implement core system architecture and data management for International Trade Fair application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,17 @@
 module keya.internationaltradefairltd {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.base;
 
     opens keya.internationaltradefairltd to javafx.fxml;
     opens keya.internationaltradefairltd.EventManager to javafx.fxml;
     opens keya.internationaltradefairltd.CustomerSupportAgent to javafx.fxml;
-    opens keya.internationaltradefairltd.HelperClass to javafx.base;
-
-
-
-
-
+    opens keya.internationaltradefairltd.HelperClass to javafx.base, javafx.fxml;
+    opens keya.internationaltradefairltd.User to javafx.base, javafx.fxml;
 
     exports keya.internationaltradefairltd;
-
+    exports keya.internationaltradefairltd.EventManager;
+    exports keya.internationaltradefairltd.CustomerSupportAgent;
+    exports keya.internationaltradefairltd.HelperClass;
+    exports keya.internationaltradefairltd.User;
 }
