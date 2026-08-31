753595443edd5fd6e7e2604136c7c081f00897c9
Abila Khan <khankeya961@gmail.com>
2026-08-31 22:00:01 +0600

    Update EventManager.java
    feat: implement core system architecture and data management for International Trade Fair application

--- src/main/java/keya/internationaltradefairltd/User/EventManager.java
+++ src/main/java/keya/internationaltradefairltd/User/EventManager.java
@@ -1,15 +1,14 @@
 package keya.internationaltradefairltd.User;
 
-public class  EventManager extends User {
+public class EventManager extends User {
+    private static final long serialVersionUID = 1L;
 
     public EventManager() {
+        super();
+        setUserType("EventManager");
     }
 
-    public EventManager(String firstName, String lastName, String password, String confirmPassword, String phoneNumber, String userType) {
-        super(firstName, lastName, password, confirmPassword, phoneNumber, userType);
-    }
-
-    public void creatReg( String orgName, String firstName,String lastName,String userName,String phoneNumber,String password,String description,String request){
-
+    public EventManager(String firstName, String lastName, String userName, String password, String confirmPassword, String phoneNumber) {
+        super(firstName, lastName, userName, password, confirmPassword, phoneNumber, "EventManager");
     }
 }
